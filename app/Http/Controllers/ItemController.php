9773f6f1b52630200915f658b3660f1c4b879d04
Dale <tayagdale@gmail.com>
2024-01-10 07:33:46 +0800

    adjust update item validation

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -81,7 +81,8 @@ class ItemController extends Controller
         $request->validate([
             'category_id' => 'required|integer',
             'generic_name' => 'required|string|min:3|max:20',
-            'brand_name' => 'required|string|min:3|max:20',
+            // 'brand_name' => 'required|string|min:3|max:20',
+            'brand_name' => 'required|string|min:3|max:20|unique:items,brand_name,NULL,id,generic_name,' . $request->input('generic_name'),
             'edited_by' => 'required|integer',
             // Add validation rules for other attributes as needed
         ]);
